fc8a30db9ac95472f797468a1a3a1d9311675b4f
JoonPyoJang <jjpyo1009@gmail.com>
2022-11-10 02:26:51 +0900

    Question : 10번 문제 풀이

---
+++ week_1/Question_no_10.py
@@ -0,0 +1,37 @@
+'''
+문제 설명
+수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+
+마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
+completion의 길이는 participant의 길이보다 1 작습니다.
+참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+참가자 중에는 동명이인이 있을 수 있습니다.
+
+풀기전 생각하기
+for 문으로 리스트에 있는 사람들을 하나씩 확인하고 있다면 해당 사람을 지우고 for 문을 break 시킨다. 
+'''
+
+''' 내가 풀었지만 시간 복잡도가 많이 걸린다.
+def solution(participant, completion):
+    for i in completion:
+        participant.remove(i)
+    answer = participant[0]
+    return answer
+
+
+participant = ["mislav", "stanko", "mislav", "ana"]
+completion = ["stanko", "ana", "mislav"]
+
+print(solution(participant, completion))
+
+'''
+class Dict:
+    def __init__(self):
+        self.items = [None] * 8
+
+    def put(self, key, value):
+        # 구현해보세요!
+        return
