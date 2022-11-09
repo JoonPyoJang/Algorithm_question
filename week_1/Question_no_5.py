b06ca2a7bde4f134d59591c900bc2716a0cd08cf
JoonPyoJang <jjpyo1009@gmail.com>
2022-11-09 19:03:36 +0900

    Question : 5번 문제 풀이

---
+++ week_1/Question_no_5.py
@@ -0,0 +1,54 @@
+'''
+문제 설명
+
+네오와 프로도가 숫자놀이를 하고 있습니다. 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.
+
+다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
+
+1478 → "one4seveneight"
+234567 → "23four5six7"
+10203 → "1zerotwozero3"
+이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다. s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
+
+참고로 각 숫자에 대응되는 영단어는 다음 표와 같습니다.
+
+|숫자|영단어|
+|0|zero|
+|1|one|
+|2|two|
+|3|three|
+|4|four|
+|5|five|
+|6|six|
+|7|seven|
+|8|eight|
+|9|nine|
+
+풀기전 생각하기
+문자열에서 숫자별 영단어를 key 값으로 정하고 for 문을 돌려 value 값에 있는 단어가 있는지 찾는다 만약 있다면 단어를 key값으로 변환을 한다
+'''
+
+def solution(s):
+    num = {
+        0:'zero',
+        1:'one',
+        2:'two',
+        3:'three',
+        4:'four',
+        5:'five',
+        6:'six',
+        7:'seven',
+        8:'eight',    
+        9:'nine'
+    }
+
+    for i in range(10):
+        if num[i] in s:
+            s= s.replace(num[i] ,str(i))
+
+    answer = int(s)
+
+    return answer
+
+print(solution("23four5six7"))
+
