1029b2c534d21bba5475b267f21e4fe75025fd88
JoonPyoJang <jjpyo1009@gmail.com>
2022-11-20 18:29:33 +0900

    Question : 2주차 7번 문제 풀이

--- week_2/question_no_8.py
+++ week_2/question_no_8.py
@@ -1,9 +1,11 @@
+'''
+문제 설명
 
-# sorted
-arr = [5, 8, 1, 2, 4, 9, 3, 7, 6]
-print("sorted data: ", sorted(arr))
-print("input data: ", arr)
-# sort()
-arr = [5, 8, 1, 2, 4, 9, 3, 7, 6]
-arr.sort()
-print("input data: ", arr)
+
+제한사항
+
+
+풀기전 생각해보기
+
+'''
+
